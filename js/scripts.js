a60341448eab2ad72d478f575f0f2f4e0edee048
Dallas Bass <dallasb86@gmail.com>
2016-04-03 16:10:22 -0500

    Completion of the homepage and the archive page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log("hi bob");
+$( document ).ready(function() {
+    $('.top-posts ol:not(:first)').hide();
+    $('.top-posts p').click(function() {
+    	if ($(this).next('ol').is(":visible"))  {
+    		return false;
+    	} else {
+    		$('.top-posts ol').slideUp();
+    	}
+    	$(this).next('ol').slideDown();
+    	$('.top-posts p').removeClass('top-posts-open');
+    	$(this).addClass('top-posts-open');
+    })
+
+    $('.mobile-nav-open').click(function() {
+    	$(this).find('i').toggleClass('fa-bars fa-times');
+    	$('.mobile-nav').slideToggle();
+    })
+});
